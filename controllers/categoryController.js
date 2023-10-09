29f944ea2cfd8aab3c397151c66dcebc738d5d72
Irina Batischeva <batischeva@bk.ru>
2023-10-09 12:25:50 +0300

    category & position routers & controllers added

---
+++ controllers/categoryController.js
@@ -0,0 +1,10 @@
+class CategoryController {
+  async create(req, res) {
+
+  }
+  async getAll(req, res) {
+    
+  }
+}
+
+module.exports = new CategoryController();
--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,14 @@ const userRouter = require('./userRouter.js');
 const requestRouter = require('./requestRouter.js');
 const agentRouter = require('./agentRouter.js');
 const clientRouter = require('./clientRouter.js');
+const positionRouter = require('./positionRouter.js');
+const categoryRouter = require('./categoryRouter.js');
 
 router.use('/user', userRouter);
 router.use('/request', requestRouter);
 router.use('/agent', agentRouter);
 router.use('/client', clientRouter);
+router.use('/position', positionRouter);
+router.use('/category', categoryRouter);
 
 module.exports = router;
