73828ed2f0a87769d6645a68863a16b270ae041c
Irina Batischeva <batischeva@bk.ru>
2023-10-09 22:18:47 +0300

    pagination added

--- controllers/requestController.js
+++ controllers/requestController.js
@@ -12,30 +12,37 @@ class RequestController {
     }
   }
   async getAll(req, res) {
-    const {agentId, clientId, statusId, categoryId, priorityId} = req.query;
+    let {agentId, clientId, statusId, categoryId, priorityId, limit, page} = req.query;
+
+    page = page || 1;
+    limit = limit || 10;
+    let offset = page * limit - limit;
+    
     let requests;
     if (!agentId && !clientId && !statusId && !categoryId && !priorityId) {
-      requests = await Request.findAll();
+      requests = await Request.findAndCountAll({limit, offset});
     }
     if (agentId && !clientId && !statusId && !categoryId && !priorityId) {
-      requests = await Request.findAll({where: {agentId}});
+      requests = await Request.findAndCountAll({where: {agentId}, limit, offset});
     }
     if (!agentId && clientId && !statusId && !categoryId && !priorityId) {
-      requests = await Request.findAll({where: {clientId}});
+      requests = await Request.findAndCountAll({where: {clientId}, limit, offset});
     }
     if (!agentId && !clientId && statusId && !categoryId && !priorityId) {
-      requests = await Request.findAll({where: {statusId}});
+      requests = await Request.findAndCountAll({where: {statusId}, limit, offset});
     }
     if (!agentId && !clientId && !statusId && categoryId && !priorityId) {
-      requests = await Request.findAll({where: {categoryId}});
+      requests = await Request.findAndCountAll({where: {categoryId}, limit, offset});
     }
     if (!agentId && !clientId && !statusId && !categoryId && priorityId) {
-      requests = await Request.findAll({where: {priorityId}});
+      requests = await Request.findAndCountAll({where: {priorityId}, limit, offset});
     }
     return res.json(requests);
   }
   async getOne(req, res) {
-    
+    const {id} = req.params;
+    const request = await Request.findOne({where: {id}});
+    return res.json(request);
   }
 }
 
